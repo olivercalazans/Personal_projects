4b5d2bb72b8d29b788870e3bdafcd87a9c8be2dd
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-05-04 18:58:26 -0300

    Update Fragmentação de datagramas (DESENVOLVENDO).py

--- Fragmentação de datagramas (DESENVOLVENDO).py
+++ Fragmentação de datagramas (DESENVOLVENDO).py
@@ -1,26 +1,33 @@
-# Esse código faz os cálculos de uma fragmentação de datagramas do IPV4. 
-
 # OBS: AINDA ESTOU DESENVOLVENDO A REFRAGMENTAÇÃO. Por enquanto ele só faz os cálculos para uma fragmentação.
 
-quantidade_dados_ini = int(input('\nQuantidade de dados em bytes: '))
-tamanho_cabecalho    = int(input('Tamanho do cabeçalho em bytes: '))
-quantidade_enlaces   = int(input('Quantidade de enlaces: '))
+# Código feito para calcular a fragmentação de datagramas.
+#-------------------------------------------------------------------------
+
+quantidade_dados_ini = int(input('\nQuantidade de dados em bytes...: '))
+tamanho_cabecalho    = int(input('Tamanho do cabeçalho em bytes..: '))
+quantidade_enlaces   = int(input('Quantidade de enlaces..........: '))
 mtus = []
 for turn in range(quantidade_enlaces):
     capacidade_mtu = int(input('Informe a MTU dos enlaces: '))
     mtus.append(capacidade_mtu)
+print('\n', 100 * '-')
+
+lista_de_fragmentos = [] # Lista feita para armazenar informações e para o funcionamento do código.
+                         # Ordem da lista: FRAGMENTO, MORE FRAGMENT, HLEN, DATA, TOTAL LENGTH, OFFSET.
 
 for mtu in mtus:
-    mtu = mtu - 20 # Subitraindo o cabeçalho.
-    mtu = mtu // 8 # Descobrindo o offset.
+    mtu = mtu - 20 # Subtraindo o cabeçalho.
+    mtu = mtu // 8 # Obtendo o offset.
     offset = mtu
-    mtu = mtu * 8 # Descobrindo a quantidade de dados por pacote/datagrama.
+    mtu = mtu * 8 # Obtendo a quantidade de dados por pacote/datagrama.
     
     # Quantidade de fragmentos. Resultado fragmentado, adiciona mais um fragmento.
     quantidade_fragmentos = quantidade_dados_ini // mtu 
     if quantidade_dados_ini % mtu > 0:
         quantidade_fragmentos += 1
     
+    fragmento_salvar = []
+
     print('\n')
     quantidade_dados = quantidade_dados_ini
     fragmento = 1
@@ -35,7 +42,17 @@ for mtu in mtus:
             quantidade_dados -= mtu
             dados_do_pacote = mtu
         
-        print(f'FRAGMENTO {fragmento}:\n')
+        fragmento_interno = []
+        fragmento_interno.append(fragmento)
+        fragmento_interno.append(mf)
+        fragmento_interno.append(tamanho_cabecalho)
+        fragmento_interno.append(dados_do_pacote)
+        fragmento_interno.append(dados_do_pacote + tamanho_cabecalho)
+        fragmento_interno.append(offset_do_datagrama)
+
+        fragmento_salvar.append(fragmento_interno)
+
+        print(f'\nFRAGMENTO {fragmento}:\n')
         print(f'MORE FRAGMENT.........: {mf}')
         print(f'HLEN (BYTES)..........: {tamanho_cabecalho}')
         print(f'DATA (BYTES)..........: {dados_do_pacote}')
@@ -44,3 +61,5 @@ for mtu in mtus:
         print(100 * '-')
         fragmento += 1
         offset_do_datagrama += offset
+
+    lista_de_fragmentos.append(fragmento_salvar)
