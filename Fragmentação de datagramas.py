a6972cd0c19bffb3e3738ca7fa226b750e1f48b9
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-05-13 00:24:46 -0300

    Update Fragmentação de datagramas.py

--- Fragmentação de datagramas.py
+++ Fragmentação de datagramas.py
@@ -20,7 +20,7 @@ def verificador_de_valor(valor):
         else:
             return valor
 
-# Descobrindo qual o protocolo que sera usado.
+# Descobrindo qual o PROTOCOLO que sera usado.
 while True:
     print('\n1 - IPV4')
     print('2 - IPV6')
@@ -31,11 +31,11 @@ while True:
     else:
         print('ESCOLHA APENAS 1 OU 2.')
 
-# Recebendo a quantidade de dados que serão transportados.
+# Recebendo a QUANTIDADE DE DADOS que serão transportados.
 mensagem  = '\nQuantidade de dados: '
 quantidade_dados_ini = verificador_de_valor(quantidade_dados_ini)
 
-# Recebendo o tamanho do cabeçalho. O IPV6 tem um valor fixo de 40.
+# Recebendo o tamanho do CABEÇALHO. O IPV6 tem um valor fixo de 40.
 if protocolo == 2:
     cabecalho = 40
 else:
@@ -47,7 +47,7 @@ else:
         else:
             print('O cabeçalho do IPV4 fica entre 20 e 60.')
 
-# Recebendo quantos enlaces/nós existem no caminho.
+# Recebendo quantos ENLACES/NÓS existem no caminho.
 mensagem  = '\nQuantidade de enlaces/nós: '
 quantidade_enlaces = verificador_de_valor(quantidade_enlaces)
 
@@ -74,86 +74,81 @@ def print_dos_dados(more_fragment, tamanho_cabecalho, dados_do_pacote, total_len
     print(f'FRAGMENT OFFSET.......: {offset_do_datagrama}')
     print(100 * '-')
 
+# Dependendo do protocolo escolhido, o código seguirá um de dois caminhos.
+# O protocolo IPV6 fragmenta os dados de acordo com a menor MTU entre a origem e o destino.
+# O protocolo IPV4 faz a fragmentação de no caminho, de acordo com a MTU que aparece.
 
+# Se o valor da variável for 2, o protocolo escolhido foi o IPV6.
 if protocolo == 2:
     more_fragment = 1
     menor_mtu     = min(mtus)
     area_de_dados = ((menor_mtu - cabecalho) // 8) * 8
     dados_transportados = 0
     while quantidade_dados_ini > 0:
+        # Se a quantidade de dados for MENOR OU IGUAL a área de dados do datagrama.
         if quantidade_dados_ini <= area_de_dados:
-            more_fragment   = 0
-            dados_do_pacote = quantidade_dados_ini
+            more_fragment         = 0
+            dados_do_pacote       = quantidade_dados_ini
             quantidade_dados_ini -= quantidade_dados_ini
-            total_len = dados_do_pacote + cabecalho
-            offset_do_datagrama = dados_transportados // 8
+            total_len             = dados_do_pacote + cabecalho
+            offset_do_datagrama   = dados_transportados // 8
             print_dos_dados(more_fragment, cabecalho, dados_do_pacote, total_len, offset_do_datagrama)
+        # Se a quantidade de dados for MAIOR que a área de dados do datagrama.
         else:
-            more_fragment = 1
+            more_fragment         = 1
             quantidade_dados_ini -= area_de_dados
-            dados_do_pacote = area_de_dados
-            total_len = dados_do_pacote + cabecalho
-            offset_do_datagrama = dados_transportados // 8
+            dados_do_pacote       = area_de_dados
+            total_len             = dados_do_pacote + cabecalho
+            offset_do_datagrama   = dados_transportados // 8
+            dados_transportados  += dados_do_pacote
             print_dos_dados(more_fragment, cabecalho, dados_do_pacote, total_len, offset_do_datagrama)
-            dados_transportados += dados_do_pacote
+
+# Se o valor da variável for 1, o protocolo escolhido foi o IPV4.
 else:
     enlace   = 1
     dados    = [quantidade_dados_ini + cabecalho]  # Lista usada para armazenar dados para a fragmentação. Ela tem seus valores subistituidos a cada laço.
     dados_para_subir = list()  # Lista que substituirá a lista "dados".
+    # Uma MTU é escolhida para que os cálculos sejam feitos.
     for mtu in mtus:
         print(f'{enlace}° enlace (MTU de {mtu})\n')
-        area_de_dados = ((mtu - cabecalho) // 8) * 8   # Obtendo a quantidade de dados por pacote/datagrama.
-
-        more_fragment = 1
-        contador_de_loop  = 1 
-        ultimo_loop       = len(dados)
-        dados_transportados   = 0
-        ultimo_confirmado = False
-        for index in dados:
-            if contador_de_loop == ultimo_loop:
+        more_fragment = contador_de_loop = 1
+        area_de_dados = ((mtu - cabecalho) // 8) * 8
+        ultimo_confirmado   = False
+        dados_transportados = 0  # Valor usado para calcular o offset.
+        # De acordo com a MTU escolhida, todos os datagramas seram comparados para saber se tem o tamanho adequado.
+        for indice in dados:
+            if contador_de_loop == len(dados):
                 ultimo_confirmado = True
-            if index <= mtu:
-                dados_do_pacote = index - cabecalho
-                total_len = index
+            # Entraram nessa parte os datagramas que são MENORES OU IGUAIS a MTU.
+            if indice <= mtu:
                 if ultimo_confirmado == True:
                     more_fragment = 0
-                offset_do_datagrama = dados_transportados // 8
-                print(f'MORE FRAGMENT.........: {more_fragment}')
-                print(f'HLEN (BYTES)..........: {cabecalho}')
-                print(f'DATA (BYTES)..........: {dados_do_pacote}')
-                print(f'TOTAL LENGTH (BYTES)..: {total_len}')
-                print(f'FRAGMENT OFFSET.......: {offset_do_datagrama}')
-                print(100 * '-')
-                dados_para_subir.append(index)
+                dados_do_pacote      = indice - cabecalho
+                total_len            = indice
+                offset_do_datagrama  = dados_transportados // 8
                 dados_transportados += dados_do_pacote
+                dados_para_subir.append(indice)
+                print_dos_dados(more_fragment, cabecalho, dados_do_pacote, total_len, offset_do_datagrama)
+            # A entraram os datagramas que são MAIORES que a MTU para seren refragmentados.
             else:
-                index -= cabecalho
-                while index > 0:
+                indice -= cabecalho
+                while indice > 0:
                     offset_do_datagrama = dados_transportados // 8
-                    if index <= area_de_dados:
-                        dados_do_pacote = index
-                        total_len = dados_do_pacote + cabecalho
-                        index -= index
+                    if indice <= area_de_dados:
+                        dados_do_pacote = indice
+                        total_len       = dados_do_pacote + cabecalho
+                        indice         -= indice
                         if ultimo_confirmado == True:
                             more_fragment = 0
                     else:
-                        index -= area_de_dados
+                        indice         -= area_de_dados
                         dados_do_pacote = area_de_dados
-                        total_len = area_de_dados + cabecalho
-                
-                    dados_para_subir.append(total_len)
-
-                    print(f'MORE FRAGMENT.........: {more_fragment}')
-                    print(f'HLEN (BYTES)..........: {cabecalho}')
-                    print(f'DATA (BYTES)..........: {dados_do_pacote}')
-                    print(f'TOTAL LENGTH (BYTES)..: {total_len}')
-                    print(f'FRAGMENT OFFSET.......: {offset_do_datagrama}')
-                    print(100 * '-')
-
+                        total_len       = area_de_dados + cabecalho
                     dados_transportados += dados_do_pacote
+                    dados_para_subir.append(total_len)
+                    print_dos_dados(more_fragment, cabecalho, dados_do_pacote, total_len, offset_do_datagrama)
             contador_de_loop += 1
-        
         enlace   += 1
-        dados = list()
-        dados = dados_para_subir
-        dados_para_subir = list()
+        dados = list()  # Esvaziando lista para receber os valores novos. 
+        dados = dados_para_subir  # Recebendo os valores novos da lista secondaria.
+        dados_para_subir = list()  # Esvaziando a lista.
