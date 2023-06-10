8a2c2eaf62ba15ee293f3012f1e2b7f814db264d
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-06-09 21:03:21 -0300

    Update host_by_name.py

--- host_by_name.py
+++ host_by_name.py
@@ -4,7 +4,11 @@ host = input('\nInforme o host: ')
 
 try:
     ip = socket.gethostbyname(host)
+    print('-' * 50)
+except socket.gaierror:
+    print('\nErro no nome do host.')
 except:
     print(f'ERRO...:{sys.exc_info()[0]}')
 else:
     print(ip)
+    print('-' * 50)
