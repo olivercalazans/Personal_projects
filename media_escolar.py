ad7db4bb0cefceec76db0148d3ffec474813b2d3
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-06-21 00:55:48 -0300

    Update media_escolar.py

--- media_escolar.py
+++ media_escolar.py
@@ -1,10 +1,10 @@
-# Fiz esse código para me ajudar a calcular a média dos alunos. 
+# Esse código recebe os nomes, dos alunos, 4 notas e tira a média das notas. Ao final é possível criar um arquivo
+# com os dados gerados. Antes de escrever, os dados são mostrados para que possam ser conferidos antes de serem salvos.
 
-import sys
+import sys, os
 
-#------------------------------------------------------------------------------------
-# Função para evitar a repetição dos mesmos comandos.
-# Essa função aceita apenas erro.
+# Função para evitar a repetição dos mesmos comandos. ------------------------------------------------------------------
+# Essa função aceita apenas números dentro do limite estabelecido.
 
 def verificador_valor():
     while True:
@@ -18,15 +18,25 @@ def verificador_valor():
         except:
             print(f'ERRO...:{sys.exc_info()[0]}')
         else:
-            return valor
-#------------------------------------------------------------------------------------
+            if valor < 0 or valor > limite:
+                print('NOTA FORA DO LIMITE!!!')
+            else:
+                return valor
 
-data = list() # Lista que armazena os dados (nome, 1° nota, 2° nota, 3° nota, 4° nota, média)
+# Área de armazenamento -----------------------------------------------------------------------------------------------
+# "data" - lista que salva os dados de entrada.
+# "data_wrt" - lista usada para salvar todos os dados (incluindo a média) e escrever o arquivo.
 
+data     = list()
+data_wrt = list()
+
+# Entrada de dados ----------------------------------------------------------------------------------------------------
+# Recebendo o número da turma/serie.
+limite   = 10
 mensagem = 'Digite o número da turma: '
 serie    = verificador_valor()
 
-while True:
+while True:   # Recebendo os nomes e as 4 notas do aluno e calculando a média.
     linha = list()
     nome  = input('\nNome do aluno(a)(p/ sair "exit"): ')
     if nome == 'exit': break
@@ -37,6 +47,7 @@ while True:
         linha.append(nota)
     data.append(linha)
 
+# Apresentação --------------------------------------------------------------------------------------------------------
 print('-' * 100)
 print(f'\n{serie:.0f}° Ano        Nota 1       |Nota 2       |Nota 3       |Nota 4       |Média')
 for nome in data:
@@ -44,6 +55,46 @@ for nome in data:
     for dado in nome:  # Laço para descobrir o espaço entre os dados. Isso mantem os dados organizados para a escrita.
         aux = 13 - len(str(dado))
         espaco.append(aux)
+    media = (nome[1] + nome[2] + nome[3] + nome[4]) / 4
+    nome.append(media)
+    full_line = f'{nome[0]}{f"." * espaco[0]} {nome[1]}{" " * espaco[1]}|{nome[2]}{" " * espaco[2]}|{nome[3]}{" " * espaco[3]}|{nome[4]}{" " * espaco[4]}|{nome[5]:.1f}'
+    data_wrt.append(full_line)
+    print(full_line)
+print('\n')
+print('-' * 100)
 
-    print(f'{nome[0]}{f"." * espaco[0]} {nome[1]}{" " * espaco[1]}|{nome[2]}{" " * espaco[2]}|{nome[3]}{" " * espaco[3]}|{nome[4]}{" " * espaco[4]}|{(nome[1] + nome[2] + nome[3] + nome[4]) / 4:.1f}')
-   
+# Salvando arquivo com os dados apresentados --------------------------------------------------------------------------
+while True:
+    permissao = input('\nDeseja salvar um arquivo? Sim(s) / Não(n): ').upper()
+    if permissao == 'S' or permissao == 'N': break
+
+if permissao == 'S':
+    # Criando uma pasta "notas" para armazenar o arquivo.
+    DIRETORIO  = os.path.dirname(os.path.abspath(__file__))
+    DIRETORIO += '\\notas\\'
+
+    try:
+        os.mkdir(DIRETORIO)
+    except FileExistsError:
+        print('\nO diretóro já existe.\n')
+    except:
+        print(f'ERRO...:{sys.exc_info()[0]}')
+    else:
+        print(f'\nDiretório criado com sucesso!!!\n')
+    
+    # Recebendo o número do bimestre.
+    limite   = 4
+    mensagem = ('Insira o número do bimestre: ')
+    bimestre = verificador_valor()
+
+    # Criando o arquivo.
+    try:
+        with open(DIRETORIO + f'{serie:.0f}° Ano - {bimestre:.0f}° bimestre.txt','w',encoding='utf-8') as escrevendo:
+            escrevendo.write(f'{serie:.0f}° Ano        Nota 1       |Nota 2       |Nota 3       |Nota 4       |Média\n')
+            for dados in data_wrt:
+                escrevendo.write(f'{dados}\n')
+    except:
+        print(f'\nERRO...:{sys.exc_info()[0]}')
+        sys.exit()
+    else:
+        print('\nArquivo salvo com sucesso!!!')
