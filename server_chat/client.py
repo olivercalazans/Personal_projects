1abab24662daa802abee2ccd00a29e951add656b
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-08-09 23:58:01 -0300

    Finalização das condições e laços.
    Finalização de condições e laços que interagem com o servidor para que sejam feitos o login e/ou a criação de novas contas.

--- server_chat/client.py
+++ server_chat/client.py
@@ -1,37 +1,29 @@
 import socket
 
 # =================================== CONSTANTS ==================================
-
 BUFFER = 1024
-
 # ================================================================================
 
 socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socketClient.connect(('localhost', 50000))
 
 while True:
-    registerOrLogin = input('0 - Register\n1 - Login\nWrite the number: ')
+    registerOrLogin = input('\n0 - Register\n1 - Login\nWrite the number: ')
     socketClient.send(registerOrLogin.encode())
     if registerOrLogin == '0':
-        while True:
-            nameAndPassword = str((input('Write your name: '), input('Write a password: ')))
-            socketClient.send(nameAndPassword.encode())
-            confirmation = socketClient.recv(BUFFER).decode()
-            if confirmation == 'ok':
-                print('Registration completed')
-                break
-            else:
-                print(f'The name {nameAndPassword[0]} is not available')
+        nameAndPassword = (input('Write your name: '), input('Write a password: '))
+        socketClient.send(str(nameAndPassword).encode())
+        confirmation = socketClient.recv(BUFFER).decode()
+        if confirmation == 'ok':
+            print('Registration completed')
+        else:
+            print(f'The name "{nameAndPassword[0]}" is not available')
     elif registerOrLogin == '1':
-        while True:
-            nameAndPassword = str((input('Name: '), input('Password: ')))
-            socketClient.send(nameAndPassword.encode())
-            confirmation = socketClient.recv(BUFFER).decode()
-            if confirmation == 'ok':
-                break
-            else:
-                print('Name or password is wrong')
-
-        
-   
-
+        nameAndPassword = (input('Name: '), input('Password: '))
+        socketClient.send(str(nameAndPassword).encode())
+        confirmation = socketClient.recv(BUFFER).decode()
+        if confirmation == 'ok':
+            print(f'Welcome, {nameAndPassword[0]}')
+            break
+        else:
+            print('Name or password is wrong')
