9a985b39afc6907f3af2fb3cf273e73c52e54169
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-08-23 22:55:08 -0300

    Alteração de nomes e separação de funções.
    Alteração dos nomes das funções para melhor compreensão e dividindo uma função em duas para melhorar o entendimento de cada função.

--- server_chat/server.py
+++ server_chat/server.py
@@ -8,43 +8,41 @@ BUFFER              = 1024
 TODAYS_DATE         = str(datetime.date.today())
 DIRECTORY           = os.path.dirname(os.path.abspath(__file__))
 # ============================================ FUNCTIONS =================================================
-# Function to deactivate the server.
 def deactivating():
-    writing_history()
+    writingHistory()
     sys.exit()
 
-# Function for the creation of important folders and file.
-def creator_or_reader(folderName):
-    global NAMES_AND_PASSWORDS
+def creatingFoldersAndFile(folderName):
     try:
-        if folderName != 'read_data': print(f'Creating "{folderName}"')
+        print(f'Creating "{folderName}"')
         if folderName == 'names_and_passwords.txt':
             if os.path.exists(DIRECTORY + '\\CLIENTS\\' + folderName): raise FileExistsError
             else:
                 with open(DIRECTORY + '\\CLIENTS\\' + folderName, 'w'): pass
-        elif folderName == 'read_data':
-            print('Reading "names_and_passwords.txt"')
-            with open(DIRECTORY + '\\CLIENTS\\' + 'names_and_passwords.txt', 'r', encoding='utf-8') as lines: NAMES_AND_PASSWORDS = lines.read().split('\n')
         else: os.mkdir(DIRECTORY + folderName)
     except FileExistsError: 
         print(f'  --> "{folderName}" already exists')
         HISTORY.append((TODAYS_DATE, f'creating {folderName}', 'FileExistsError'))
+        if folderName == 'names_and_passwords.txt': readingTheFileOfNamesAndPasswords()
     except: 
         print(f'\nERROR...:{sys.exc_info()[0]}')
         HISTORY.append((TODAYS_DATE, f'creating {folderName}', {sys.exc_info()[0]}))
     else:
-        if folderName != 'read_data': print(f'  --> "{folderName}" was created')
-        else: print('  --> Extracted data')
+        print(f'  --> "{folderName}" was created')
         HISTORY.append((TODAYS_DATE, f'creating {folderName}', 'Successfully created'))
 
-# Writing history
-def writing_history():
+def readingTheFileOfNamesAndPasswords():
+    global NAMES_AND_PASSWORDS
+    print('Reading "names_and_passwords.txt"')
+    with open(DIRECTORY + '\\CLIENTS\\' + 'names_and_passwords.txt', 'r', encoding='utf-8') as lines: NAMES_AND_PASSWORDS = lines.read().split('\n')
+    print('  --> Extracted data')
+
+def writingHistory():
     with open(DIRECTORY + '\\CLIENT\\' + 'names_and_passwords.txt', 'w', encoding='utf-8') as pen:
         for line in HISTORY: pen.write(line)
         HISTORY = list()
 
-# Creating a new account or logging in
-def register_or_login(connection, client):
+def loggingInOrCreatingAnAccount(connection, client):
     try:
         while True:
             registerOrLogin = connection.recv(BUFFER).decode()
@@ -69,10 +67,11 @@ def register_or_login(connection, client):
             elif registerOrLogin == '1':
                 nameAndPassword = connection.recv(BUFFER).decode()
                 if nameAndPassword in NAMES_AND_PASSWORDS:
-                    nameAndPassword = eval(nameAndPassword)
                     connection.send('ok'.encode())
-                    HISTORY.append((TODAYS_DATE, 'logging in', 'successful', f'{nameAndPassword[0]}:{client}'))
-                    print(f'login: {nameAndPassword[0]} {client}')
+                    HISTORY.append((TODAYS_DATE, 'logging in', 'successful', f'{eval(nameAndPassword)[0]}:{client}'))
+                    print(f'login: {eval(nameAndPassword)[0]} {client}')
+                    tCHATS = threading.Thread(target=chats, args=(connection, client,))
+                    tCHATS.start()
                     break
                 else:
                     connection.send('wrong'.encode())
@@ -81,12 +80,15 @@ def register_or_login(connection, client):
         print(f'\nERROR...:{sys.exc_info()[0]}')
         HISTORY.append((TODAYS_DATE, client, f'\nERROR...:{sys.exc_info()[0]}'))
 
+def chats(connetion, client):
+    ...
+
+
 # ========================================================================================================
 
-creator_or_reader('\\HISTORY\\')
-creator_or_reader('\\CLIENTS\\')
-creator_or_reader('names_and_passwords.txt')
-creator_or_reader('read_data')
+creatingFoldersAndFile('\\HISTORY\\')
+creatingFoldersAndFile('\\CLIENTS\\')
+creatingFoldersAndFile('names_and_passwords.txt')
 
 try:
     socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -103,7 +105,7 @@ else:
 try:
     while True:
         connection, client = socketServer.accept()
-        tREGISTER_LOGIN = threading.Thread(target=register_or_login, args=(connection, client,))
+        tREGISTER_LOGIN = threading.Thread(target=loggingInOrCreatingAnAccount, args=(connection, client,))
         tREGISTER_LOGIN.start()
         HISTORY.append((TODAYS_DATE, 'connection', client))
 except:
