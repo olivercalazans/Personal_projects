57dd7955101125e8db0425ee1dac21da75f7bffa
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-09-01 11:27:35 -0300

    Divisão da função "loggingInOrCreatingAnAccount"
    Divisão da função "loggingInOrCreatingAnAccount" para facilitar na leitura e manutenção do código. Foram criadas as funções "loggingIn" e "creatingAnAccount".

--- server_chat/functions.py
+++ server_chat/functions.py
@@ -45,40 +45,51 @@ def loggingInOrCreatingAnAccount(connection, client):
     try:
         while True:
             registerOrLogin = connection.recv(BUFFER).decode()
-            # Registration
             if registerOrLogin == '0':
-                nameAndPassword = connection.recv(BUFFER).decode()
-                found = False
-                if NAMES_AND_PASSWORDS != ['']:
-                    for name in NAMES_AND_PASSWORDS:
-                        if name != '' and eval(name)[0] == eval(nameAndPassword)[0]: 
-                            found = True
-                            break
-                if found == True:
-                    connection.send('wrong'.encode())
-                    HISTORY.append((TODAYS_DATE, 'creating account', 'name unavailable', client))
-                else: 
-                    NAMES_AND_PASSWORDS.append(nameAndPassword)
-                    with open(DIRECTORY + '\\CLIENTS\\' + 'names_and_passwords.txt', 'a', encoding='utf-8') as user: user.write(nameAndPassword + '\n')
-                    connection.send('ok'.encode())
-                    HISTORY.append((TODAYS_DATE, 'creating account', 'registration completed', client))
-            # Logging in
+                creatingAnAccount()
             elif registerOrLogin == '1':
-                nameAndPassword = connection.recv(BUFFER).decode()
-                if nameAndPassword in NAMES_AND_PASSWORDS:
-                    connection.send('ok'.encode())
-                    HISTORY.append((TODAYS_DATE, 'logging in', 'successful', f'{eval(nameAndPassword)[0]}:{client}'))
-                    print(f'login: {eval(nameAndPassword)[0]} {client}')
-                    tCHATS = threading.Thread(target=forwardingMessages, args=(connection, client,))
-                    tCHATS.start()
-                    writingHistory()
-                    break
-                else:
-                    connection.send('wrong'.encode())
-                    HISTORY.append((TODAYS_DATE, 'logging in', 'access denied', client, nameAndPassword))
+                loggingIn()
     except:
         print(f'\nERROR...:{sys.exc_info()[0]}')
         HISTORY.append((TODAYS_DATE, client, f'\nERROR...:{sys.exc_info()[0]}'))
+
+def creatingAnAccount(connection, client):
+    try:
+        while True:
+            nameAndPassword = connection.recv(BUFFER).decode()
+            found = False
+            if NAMES_AND_PASSWORDS != ['']:
+                for name in NAMES_AND_PASSWORDS:
+                    if name != '' and eval(name)[0] == eval(nameAndPassword)[0]: 
+                        found = True
+                        break
+            if found == True:
+                connection.send('wrong'.encode())
+                HISTORY.append((TODAYS_DATE, 'creating account', 'name unavailable', client))
+            else: 
+                NAMES_AND_PASSWORDS.append(nameAndPassword)
+                with open(DIRECTORY + '\\CLIENTS\\' + 'names_and_passwords.txt', 'a', encoding='utf-8') as user: user.write(nameAndPassword + '\n')
+                connection.send('ok'.encode())
+                HISTORY.append((TODAYS_DATE, 'creating account', 'registration completed', client))
+    except:
+        print(f'\nERROR...:{sys.exc_info()[0]}')
+        HISTORY.append((TODAYS_DATE, client, f'\nERROR...:{sys.exc_info()[0]}'))
+
+def loggingIn(connection, client):
+    while True:
+        nameAndPassword = connection.recv(BUFFER).decode()
+        if nameAndPassword in NAMES_AND_PASSWORDS:
+            connection.send('ok'.encode())
+            HISTORY.append((TODAYS_DATE, 'logging in', 'successful', f'{eval(nameAndPassword)[0]}:{client}'))
+            print(f'login: {eval(nameAndPassword)[0]} {client}')
+            tCHATS = threading.Thread(target=forwardingMessages, args=(connection, client,))
+            tCHATS.start()
+            writingHistory()
+            break
+        else:
+            connection.send('wrong'.encode())
+            HISTORY.append((TODAYS_DATE, 'logging in', 'access denied', client, nameAndPassword))
+
 
 def forwardingMessages(connection, client):
     while True:
