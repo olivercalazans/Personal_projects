5704b2d72519247382a6019cf77dc709b87a486f
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-09-06 12:38:42 -0300

    Retirada de partes do código para criar funções em outro arquivo.
    Algumas partes do código foram retiradas e postas em outro arquivo para que o código fique mais limpo. Algumas alterações foram feitas para melhorar o a fluidez do código.

--- server_chat/client.py
+++ server_chat/client.py
@@ -1,29 +1,24 @@
-import socket
+import socket, sys
+from client_functions import *
 
-# =================================== CONSTANTS ==================================
-BUFFER = 1024
-# ================================================================================
-
-socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketClient.connect(('localhost', 50000))
+try:
+    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketClient.connect(('localhost', 50000))
+except:
+    print(f'\nERROR...:{sys.exc_info()}')
+else:
+    print('\nConnected to the server')
 
 while True:
-    registerOrLogin = input('\n0 - Register\n1 - Login\nWrite the number: ')
-    socketClient.send(registerOrLogin.encode())
-    if registerOrLogin == '0':
-        nameAndPassword = (input('Write your name: '), input('Write a password: '))
-        socketClient.send(str(nameAndPassword).encode())
-        confirmation = socketClient.recv(BUFFER).decode()
-        if confirmation == 'ok':
-            print('\nRegistration completed')
-        else:
-            print(f'\nThe name "{nameAndPassword[0]}" is not available!!!')
-    elif registerOrLogin == '1':
-        nameAndPassword = (input('Name: '), input('Password: '))
-        socketClient.send(str(nameAndPassword).encode())
-        confirmation = socketClient.recv(BUFFER).decode()
-        if confirmation == 'ok':
-            print(f'\nWelcome, {nameAndPassword[0]}')
-            break
-        else:
-            print('\nName or password is wrong!!!')
+    try:
+        registerOrLogin = input('\n0 - Register\n1 - Login\nWrite the number: ')
+        socketClient.send(registerOrLogin.encode())
+        if registerOrLogin == '0':
+            tryingToCreateAnAccount(socketClient)
+        elif registerOrLogin == '1':
+            confirmation = tryingToLogIn(socketClient)
+            if confirmation == 'finished': break
+    except KeyboardInterrupt:
+        ...
+    except:
+        ...
