42e4c5bebe8bafbb3e4f7adaa3a448ebd8022e70
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-09-09 10:29:35 -0300

    Finalização das funções usadas para mandar mensagens.
    Foram finalizadas as funções que mandam as mensagens para o servidor. Houve um bug que foi corrigido no servidor.

--- server_chat/client_functions.py
+++ server_chat/client_functions.py
@@ -1,6 +1,7 @@
-
+import threading, sys
 
 BUFFER = 1024
+NAME   = ''
 
 def tryingToCreateAnAccount(socketClient):
     nameAndPassword = (input('Write your name: '), input('Write a password: '))
@@ -12,11 +13,29 @@ def tryingToCreateAnAccount(socketClient):
         print(f'\nThe name "{nameAndPassword[0]}" is not available!!!')
 
 def tryingToLogIn(socketClient):
+    global NAME
     nameAndPassword = (input('Name: '), input('Password: '))
     socketClient.send(str(nameAndPassword).encode())
     confirmation = socketClient.recv(BUFFER).decode()
     if confirmation == 'ok':
         print(f'\nWelcome, {nameAndPassword[0]}')
+        NAME = nameAndPassword[0]
+        tRECEIVING_MESSAGES = threading.Thread(target=receivingMessagesFromTheServer, args=(socketClient,))
+        tRECEIVING_MESSAGES.start()
+        tSENDING_MESSAGES = threading.Thread(target=sendingMessagesToTheServer, args=(socketClient,))
+        tSENDING_MESSAGES.start()
         return 'finished'
     else:
         print('\nName or password is wrong!!!')
+
+def receivingMessagesFromTheServer(socketClient):
+    while True:
+        print(socketClient.recv(BUFFER).decode())
+        
+def sendingMessagesToTheServer(socketClient):
+    while True:
+        try:
+            message = NAME + '> ' + input('')
+            socketClient.send(message.encode())
+        except:
+            print(f'ERROR...:{sys.exc_info()}')
